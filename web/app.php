7c60796c8f32279ad550fa4b44d2551e9c3084d3
Alexey Korolev <koroleffas@gmail.com>
2012-07-10 23:14:28 +0400

    Project's mode server's variable added.

--- web/app.php
+++ web/app.php
@@ -2,5 +2,23 @@
 use Kernel\Request;
 
 require_once __DIR__.'/../kernel/Kernel.php';
-$kernel = new Kernel\Kernel('dev');
-$kernel->boot()->start();
+$mode = getenv('PROJECT_MODE')?:'dev';
+$kernel = new Kernel\Kernel($mode);
+$kernel->boot();
+try{
+    $kernel->start();
+} catch (\Exception $e) {
+    if ($mode == 'dev'){
+        $content = '<div>' . $e->getMessage() . '</div>';
+        $content .= '<div><ul>';
+        foreach ($e->getTrace() as $call) {
+            $content .= "<li>Вызов " . $call['class'] . $call['type'] . $call['function'] .
+                (count($call['args'])>0 ? (' с параметрами: ' . implode('; ', $call['args']))
+                    : ' без параметров<br/>') .
+                ' в ' . $call['file'] . ' ' . $call['line'] . ';</li>';
+        }
+        $content .= '</ul></div>';
+        $response = new Kernel\HTTP\Response($content);
+        echo $response->sendToClient();
+    }
+}
