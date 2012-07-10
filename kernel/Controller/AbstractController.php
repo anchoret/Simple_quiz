10b9615cd3508e297292c6429d8c57aaa81e1206
Alexey Korolev <koroleffas@gmail.com>
2012-07-11 02:20:09 +0400

    Implementation base method for abstract controller

--- kernel/Controller/AbstractController.php
+++ kernel/Controller/AbstractController.php
@@ -1,6 +1,10 @@
 <?php
 namespace Kernel\Controller;
 
+use Kernel\ServiceContainer\Container;
+use Kernel\HTTP\Request;
+use Kernel\HTTP\Response;
+
 /**
  * Description of AbstractController
  *
@@ -8,4 +12,52 @@ namespace Kernel\Controller;
  */
 abstract class AbstractController {
 
+    private $container = null;
+
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    public function generateUrl($name, $options = array(), $absolute = false, $https = false)
+    {
+        return $this->get('kernel.router')
+            ->generateURl($name, $options, $absolute, $https);
+    }
+
+    public function redirect($url, $status = 302)
+    {
+       return new Response($url, $status, array('location' => $url));
+    }
+
+    public function render($view, array $options = array())
+    {
+        return new Response($this->get('kernel.template.engine')
+            ->render($view, $options));
+    }
+
+    protected function get($name)
+    {
+        return $this->container->get($name);
+    }
+
+    protected function getParameter($name)
+    {
+        return $this->container->getParameter($name);
+    }
 }
